1d9949f53a1077d1994ea91395476d329fc7da6e
Mehebul02 <mehebul2123@gmail.com>
2025-09-21 06:53:06 +0600

    Circle icon update

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,3 +1,9 @@
 import logo from "./logo.png";
+import icon1 from "./circle_icons/icon1.png";
+import icon2 from "./circle_icons/icon2.png";
+import icon3 from "./circle_icons/icon3.png";
+import icon4 from "./circle_icons/icon4.png";
+import icon5 from "./circle_icons/icon5.png";
+import icon6 from "./circle_icons/icon6.png";
 
-export { logo };
+export { logo, icon1, icon2, icon3, icon4, icon5, icon6 };
--- src/components/banner/Main_Circle.tsx
+++ src/components/banner/Main_Circle.tsx
@@ -1,60 +1,76 @@
 import { motion } from "framer-motion"
+import { icon1, icon2, icon3, icon4, icon5, icon6 } from "../../assets/images";
 const MainCircle = () => {
-    return (
-        <div>
-              <motion.div
-              animate={{ rotate: 360 }}
-              transition={{ duration: 20, repeat: Number.POSITIVE_INFINITY, ease: "linear" }}
-              className="relative w-80 h-80 lg:w-96 lg:h-96"
-            >
-              {/* Outer dotted circle */}
-              <div className="absolute inset-0 border-2 border-dashed border-gray-200 rounded-full"></div>
+  return (
+    <div>
+      <motion.div
+        animate={{ rotate: 360 }}
+        transition={{ duration: 20, repeat: Number.POSITIVE_INFINITY, ease: "linear" }}
+        className="relative w-80 h-80 lg:w-96 lg:h-96"
+      >
+        {/* Outer dotted circle */}
+        <div className="absolute inset-0 border-2 border-dashed border-gray-200 rounded-full"></div>
 
-              {/* Floating dots around the circle */}
-              <motion.div
-                initial={{ scale: 0 }}
-                animate={{ scale: 1 }}
-                transition={{ duration: 0.5, delay: 1 }}
-                className="absolute -top-2 left-1/2 transform -translate-x-1/2 w-4 h-4 bg-orange-500 rounded-full"
-              ></motion.div>
+        {/* Floating dots around the circle */}
+        <motion.div
+          initial={{ scale: 0 }}
+          animate={{ scale: 1 }}
+          transition={{ duration: 0.5, delay: 1 }}
+          className="absolute -top-4 left-1/2 transform -translate-x-1/2  "
+        >
 
-              <motion.div
-                initial={{ scale: 0 }}
-                animate={{ scale: 1 }}
-                transition={{ duration: 0.5, delay: 1.2 }}
-                className="absolute top-1/4 -right-2 w-3 h-3 bg-orange-600 rounded-full"
-              >erferf</motion.div>
+          <img src={icon1} alt="Logo" className="w-[38px] h-[38px] object-contain" />
 
-              <motion.div
-                initial={{ scale: 0 }}
-                animate={{ scale: 1 }}
-                transition={{ duration: 0.5, delay: 1.4 }}
-                className="absolute bottom-1/4 -left-2 w-3 h-3 bg-red-500 rounded-full"
-              ></motion.div>
 
-              <motion.div
-                initial={{ scale: 0 }}
-                animate={{ scale: 1 }}
-                transition={{ duration: 0.5, delay: 1.6 }}
-                className="absolute -bottom-2 right-1/3 w-4 h-4 bg-orange-400 rounded-full"
-              ></motion.div>
+        </motion.div>
 
-              <motion.div
-                initial={{ scale: 0 }}
-                animate={{ scale: 1 }}
-                transition={{ duration: 0.5, delay: 1.8 }}
-                className="absolute top-1/3 -left-3 w-2 h-2 bg-red-600 rounded-full"
-              ></motion.div>
 
-              <motion.div
-                initial={{ scale: 0 }}
-                animate={{ scale: 1 }}
-                transition={{ duration: 0.5, delay: 2 }}
-                className="absolute bottom-1/3 -right-3 w-3 h-3 bg-orange-500 rounded-full"
-              ></motion.div>
-            </motion.div>
-        </div>
-    );
+        <motion.div
+          initial={{ scale: 0 }}
+          animate={{ scale: 1 }}
+          transition={{ duration: 0.5, delay: 1.2 }}
+          className="absolute top-1/4 -right-2  "
+        > <img src={icon2} alt="Logo" className="w-[38px] h-[38px] object-contain" /></motion.div>
+
+        <motion.div
+          initial={{ scale: 0 }}
+          animate={{ scale: 1 }}
+          transition={{ duration: 0.5, delay: 1.4 }}
+          className="absolute bottom-1/4 -left-2 "
+        >
+
+          <img src={icon3} alt="Logo" className="w-[38px] h-[38px] object-contain" />
+        </motion.div>
+
+        <motion.div
+          initial={{ scale: 0 }}
+          animate={{ scale: 1 }}
+          transition={{ duration: 0.5, delay: 1.6 }}
+          className="absolute -bottom-2 right-1/3 "
+        >
+          <img src={icon4} alt="Logo" className="w-[38px] h-[38px] object-contain" />
+        </motion.div>
+
+        <motion.div
+          initial={{ scale: 0 }}
+          animate={{ scale: 1 }}
+          transition={{ duration: 0.5, delay: 1.8 }}
+          className="absolute top-1/3 -left-3 "
+        >
+          <img src={icon5} alt="Logo" className="w-[38px] h-[38px] object-contain" />
+        </motion.div>
+
+        <motion.div
+          initial={{ scale: 0 }}
+          animate={{ scale: 1 }}
+          transition={{ duration: 0.5, delay: 2 }}
+          className="absolute bottom-1/3 -right-3 "
+        >
+          <img src={icon6} alt="Logo" className="w-[38px] h-[38px] object-contain" />
+        </motion.div>
+      </motion.div>
+    </div>
+  );
 };
 
 export default MainCircle;
